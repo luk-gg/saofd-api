950a0c3f2dc57a00c3b83a764b1244db05ac80bc
Zakum <mirveo@gmail.com>
2025-02-01 10:14:57 -0800

    add elemental power to entries and /entries route

---
+++ src/lib/entries.js
@@ -0,0 +1,6 @@
+import { entries_brief as CHARACTERS } from "./characters"
+import { entries_brief as WEAPONS } from "./weapons"
+import { entries_brief as ACCESSORIES } from "./accessories"
+import { entries_brief as COSMETICS } from "./cosmetics"
+
+export default [CHARACTERS, WEAPONS, ACCESSORIES, COSMETICS].flat()
